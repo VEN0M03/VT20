958e83546df348218163155e143275aaa7c6a8f0
VEN0M03 <pythonvenom291@gmail.com>
2025-01-23 21:19:07 +0100

    Update rat.py

--- rat.py
+++ rat.py
@@ -1,10 +1,1 @@
-import os
-import platform
-os.system('clear')
-os.system("git pull")
-b = platform.architecture()[0]
-if b == '64bit':
-    import MAHA
-    
-elif b == '32bit':
-    import MAHA
+import MAHA
